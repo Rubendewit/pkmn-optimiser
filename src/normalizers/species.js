2a606331c52ea1632a8ed0e8197b2511c132c177
Ruben de Wit <ruben.de.wit@rtl.nl>
2017-12-13 15:57:03 +0100

    Calculate total base stats

--- src/normalizers/species.js
+++ src/normalizers/species.js
@@ -10,11 +10,16 @@ export const normalizeSpeciesName = ([speciesName]) => speciesName.name;
 
 export const normalizeSpeciesStats = speciesStats => {
   const stats = {};
+  let total = 0;
 
   _.forEach(speciesStats, stat => {
-    const name = statMap[stat.stat_id];
-    stats[name] = stat.base_stat;
+    const { stat_id, base_stat } = stat;
+    const name = statMap[stat_id];
+    stats[name] = base_stat;
+    total = total + base_stat;
   });
+
+  stats.total = total;
 
   return stats;
 };
