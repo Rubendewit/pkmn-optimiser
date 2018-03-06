0a7a5029d9196ed398e5ca294a26fa2fec816cd6
Ruben de Wit <ruben.de.wit@rtl.nl>
2018-03-06 13:49:50 +0100

    Directly export

--- static/smogon/index.js
+++ static/smogon/index.js
@@ -1,18 +1,7 @@
-import abilities from './abilities.json';
-import formats from './formats.json';
-import items from './items.json';
-import moves from './moves.json';
-import natures from './natures.json';
-import pokemon from './pokemon.json';
-import types from './types.json';
-
-// TODO: Find permanent solution for bulk exporting json files
-module.exports = {
-  abilities,
-  formats,
-  items,
-  moves,
-  natures,
-  pokemon,
-  types
-};
+export abilities from './abilities.json';
+export formats from './formats.json';
+export items from './items.json';
+export moves from './moves.json';
+export natures from './natures.json';
+export pokemon from './pokemon.json';
+export types from './types.json';
